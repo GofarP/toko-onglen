50469e4f2ba009189173a6015fb844efa4f2431a
Gofar.P <48397661+GofarP@users.noreply.github.com>
2022-08-13 17:04:41 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,11 +9,6 @@ $api_url = $is_production ?
   'https://app.sandbox.midtrans.com/snap/v1/transactions';
 
 
-$api_url = $is_production ? 
-  'https://app.midtrans.com/snap/v1/transactions' : 
-  'https://app.sandbox.midtrans.com/snap/v1/transactions';
-
-
 if( !strpos($_SERVER['REQUEST_URI'], '/charge') ) {
   http_response_code(404); 
   echo "wrong path, make sure it's `/charge`"; exit();
